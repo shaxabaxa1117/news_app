93f12806b0dfdcacbb8ae0247d938cd3004e04e6
Wramggler <shaxabaxa17@gmail.com>
2025-02-24 10:12:48 +0500

    Implement News API service and article repository with Dio integration

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+
+const String newsApiUrl = 'https://newsapi.org/v2';
+const String apiKey = 'dff34d6c98e048f8925190d38ed28125';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
---
+++ lib/features/daily_news/data/data_sourses/remote/news_api_service.dart
@@ -0,0 +1,26 @@
+
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/constants.dart';
+import 'package:news_app/features/daily_news/data/models/article_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'news_api_service.g.dart';
+
+
+
+@RestApi(baseUrl: newsApiUrl)
+abstract class NewsApiService {
+
+factory NewsApiService(Dio dio, {String baseUrl}) = _NewsApiService;
+
+
+ @GET('/top-headlines')
+Future<HttpResponse<List<ArticleModel>>> getNewsArticles( {
+  @Query('apiKey') String? apiKey,
+  @Query('country') String? country,
+  @Query('category') String? category, //! parameters
+});
+
+
+ //! this is going to be implemented by the retrofin generator
+}
